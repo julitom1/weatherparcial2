9b0f863c064e64add9d9320d7d3c4d82e134e727
julitom1 <yarit.villalobos@mail.escuelaing.edu.co>
2021-09-08 14:01:12 -0500

    creando servidor http y main

---
+++ src/main/java/edu/eci/httpServerWeather/HttpServerWeather.java
@@ -0,0 +1,15 @@
+package edu.eci.httpServerWeather;
+
+
+
+public class HttpServerWeather {
+
+	private static final HttpServerWeather instance=new HttpServerWeather();
+	public static Object getInstance() {
+		// TODO Auto-generated method stub
+		return instance;
+	}
+	
+	
+
+}
---
+++ src/main/java/edu/eci/weather/App.java
@@ -0,0 +1,35 @@
+package edu.eci.weather;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import edu.eci.practiceSocket.HttpServer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+public static void main(String [] args) {
+		
+		
+		try {
+			HttpServerWeather.getInstance().start(getPort());
+		}catch(IOException ex) {
+			Logger.getLogger(HttpServerWeather.class.getName()).log(Level.SEVERE,null,ex);
+		} catch (URISyntaxException ex) {
+			// TODO Auto-generated catch block
+			Logger.getLogger(HttpServerWeather.class.getName()).log(Level.SEVERE,null,ex);
+		}
+	}
+
+	private static int getPort() {
+		if(System.getenv("PORT")!=null) {
+			return Integer.parseInt(System.getenv("PORT"));
+		}
+		return 35000;
+	}
+}
